3f4695559ad1c8590160ff2c71d6245b0d023691
Kesava Devops <104878023+kesavulureddychundi@users.noreply.github.com>
2022-09-09 15:46:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+p
 }).listen(9009);
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
-app.get('/mithuntechnologies', function(request, response) {
+app.get("/mithuntechnologies", function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
-	response.set("Content-Type","text/html");
+    response.set("Content-Type","text/html");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
     response.write("<h2><center>Welcome to  Mithun Technologies. Please Contact +91-9980923226,++91-9980923216 for more information or send an email to devopstrainingblr@gmail.com <center></h2>" );
     response.end();
     
-  })
+  });
  
 
 //app.get("/html", function(req,res){
